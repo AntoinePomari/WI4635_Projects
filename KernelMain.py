1a3fb6ffa2937a6274b20e44e0486e10c94d3136
JimvdValkBouman <104001918+JimvdValkBouman@users.noreply.github.com>
2022-05-01 11:49:16 +0200

    Working/fast (final?) build

--- KernelMain.py
+++ KernelMain.py
@@ -5,10 +5,10 @@ Created on Wed Apr 20 16:06:50 2022
 @author: Antoine
 """
 import numpy as np
-import Kmeans_Kernel_Copy_Modified as KmK
-import Kmeans_BuildSimilartyMatrices as KmBSM
-
+import Kmeans_Kernel as KmK
+import Kmeans_BuildKernelMatrix as KmBKM
 from sklearn.datasets import fetch_openml
+
 X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 Xarray = X.to_numpy()
 Xarray = Xarray.astype(np.float32) / 255.0 #Normalization: useful for computation
@@ -17,18 +17,18 @@ K = 10
 
 #Set seed -> Reproducibility, random initialization
 np.random.seed(K ** 2)
-BigN = 30000
-Data = Xarray[:BigN,:]
-real_val = y[:BigN]
+BigN = 10000
+Data = Xarray[:BigN,:] 
+real_val = y[:BigN] 
 
-#Construct desired Kernel matrix
-KernelMat = KmBSM.BuildKernel(Data, kernel = "Gauss")
+#Construct desired Kernel matrix (options: Gauss, Poly2, Sigmoid, quadrant_col_sum, nnzcount)
+KernelMat = KmBKM.BuildKernel(Data, kernel = "Sigmoid")
 KernelMat = KernelMat + KernelMat.T - np.diag( np.diag(KernelMat) )
 # np.save("GaussianKernel_FullDataset",KernelMat)
 
-#NB Computations for Polynomial Kernel are very slow! -> Better keep maxit below 25 lol
-#NB Computations for Gaussian Kernel are significantly faster (not superfast tho)
-#NB Computations for Sigmoid Kernel are also very slow
 clust = KmK.Kmeans(KernelMat, Data, K, maxit = 120)
 result = KmK.MostRepresentedInEachCluster(clust, real_val)
 acc = KmK.Accuracy(clust, real_val)
+
+print("Size of resulting clusters: ", result)
+print("Accuracy: ", acc)
